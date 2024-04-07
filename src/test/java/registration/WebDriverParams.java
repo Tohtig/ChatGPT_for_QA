d07378bb33442a963a391f5ef72f9bc587197471
Anton Ippolitov <aippolitov@fil-it.ru>
2024-04-07 22:26:17 +0300

    HOMEWORK-1183 Попытка подключения селенойда. 1ый подход.

--- src/test/java/registration/WebDriverParams.java
+++ src/test/java/registration/WebDriverParams.java
@@ -2,8 +2,17 @@ package registration;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.qameta.allure.selenide.AllureSelenide;
+import helpers.Attach;
+
+import java.util.Map;
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class WebDriverParams {
 
@@ -14,6 +23,17 @@ public class WebDriverParams {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 15_000L;
+
+//        Подключение селенойда
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @BeforeEach
@@ -22,7 +42,13 @@ public class WebDriverParams {
     }
 
     @AfterEach
-    void closeWebDriver() {
-        Selenide.closeWebDriver();
+    void addAttachments() {
+        if (WebDriverRunner.getWebDriver() != null) {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+            Attach.addVideo();
+            closeWebDriver();
+        }
     }
 }
